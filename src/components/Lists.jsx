8ec79931f7065d18658e8db0b7828eb882924979
Gina González <gini_go@hotmail.com>
2024-10-10 18:39:39 +0200

    add component list

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
 function Lists() {
-  return <div>Lists</div>;
+  return (
+    <>
+      <div className="list">
+        <div className="list__item active ">
+          Vacaciones en Mallorca <span className="number">3</span>
+        </div>
+        <div className="list__item">Fiesta de cumpleaños</div>
+        <div className="list__item">Regalos de Navidad</div>
+      </div>
+    </>
+  );
 }
 
 export default Lists;
