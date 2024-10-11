6e6c572463d82945d3bdffcba74211ad23d8c0e2
Gina González <gini_go@hotmail.com>
2024-10-11 17:45:04 +0200

    add fetch list

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import AddTaskForm from "./AddTaskForm";
 import { useParams } from "react-router-dom";
 
 function TaskList() {
+  const [list, setList] = useState();
+
   const { id } = useParams();
-  console.log(id);
+
+  useEffect(() => {
+    fetch(`https://todolist-api-my16.onrender.com/lists/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setList(data);
+      });
+  }, [id]);
+
+  if (!list) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="task-list">
-      <h1 className="task-list__title">My Things To Do</h1>
+      <h1 className="task-list__title">{list.name}</h1>
       <AddTaskForm />
     </div>
   );
