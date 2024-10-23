4a7cf2ab71e229943f9263697500670a9627a71a
Gina González <gini_go@hotmail.com>
2024-10-23 23:20:37 +0200

    add new section-landingpage

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -1,6 +1,5 @@
 import { Navigate } from "react-router-dom";
 import Button from "./Button";
-import { Link } from "react-router-dom";
 
 function EmptyState({ lists }) {
   if (lists.length === 0) {
@@ -14,7 +13,6 @@ function EmptyState({ lists }) {
         <Button to="/create-list" isLink>
           Create a List
         </Button>
-        <Link to="/landing">Back to landing</Link>
       </div>
     );
   } else {
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,12 +4,8 @@ import { Link } from "react-router-dom";
 function LandingPage() {
   return (
     <div>
-      <header className="landing-header">
-        <img
-          className="landing-header__logo"
-          src="./TaskMaster.png"
-          alt="logo"
-        />
+      <header className="landing-header landing__wrapper">
+        <img className="landing-header__logo" src="./logo.png" alt="logo" />
       </header>
       <div className="hero">
         <h2 className="hero__intro">To-do list</h2>
@@ -25,32 +21,51 @@ function LandingPage() {
         </div>
       </div>
 
-      <section className="container">
-        <div className="container__info">
-          <h2 className="container__title">Why Choose TaskMaster?</h2>
-          <p className="container__desc">
-            Join thousands of productive users who trust TaskMaster to organize
-            their daily lives.
-          </p>
+      <section className="preview">
+        <div className="landing__wrapper">
+          <div className="preview-box">
+            <img className="preview-image" src="./prueba-web.png" alt="page" />
+          </div>
+          <div className="preview-text">
+            <h1 className="preview-title">
+              Creating to-do lists is quick and easy
+            </h1>
+            <p className="preview-desc">
+              Allowing you to manage everything you need to get done in just a
+              few clicks.
+            </p>
+          </div>
         </div>
-        <ul className="features">
-          <li className="feature">
-            <i className="feature__icon fa-regular fa-circle-check"></i>
-            <span className="feature__desc">Smart Organization</span>
-          </li>
-          <li className="feature">
-            <i className="feature__icon fa-solid fa-mobile-screen-button"></i>
-            <span className="feature__desc">Cross-Platform Sync</span>
-          </li>
-          <li className="feature">
-            <i className=" feature__icon fa-regular fa-clock"></i>
-            <span className="feature__desc">Time Management</span>
-          </li>
-        </ul>
+      </section>
+
+      <section className="container">
+        <div className="landing__wrapper">
+          <div className="container__info">
+            <h2 className="container__title">Why Choose TaskMaster?</h2>
+            <p className="container__desc">
+              Join thousands of productive users who trust TaskMaster to
+              organize their daily lives.
+            </p>
+          </div>
+          <ul className="features">
+            <li className="feature">
+              <i className="feature__icon fa-regular fa-circle-check"></i>
+              <span className="feature__desc">Smart Organization</span>
+            </li>
+            <li className="feature">
+              <i className="feature__icon fa-solid fa-mobile-screen-button"></i>
+              <span className="feature__desc">Cross-Platform Sync</span>
+            </li>
+            <li className="feature">
+              <i className=" feature__icon fa-regular fa-clock"></i>
+              <span className="feature__desc">Time Management</span>
+            </li>
+          </ul>
+        </div>
       </section>
 
       <footer className="landing-footer">
-        <p className="">© 2024 TaskMaster. All rights reserved.</p>
+        <p className="">© 2025 TaskMaster. All rights reserved.</p>
       </footer>
     </div>
   );
